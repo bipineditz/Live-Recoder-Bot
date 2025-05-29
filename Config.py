bfef949a291e4679bb319920e6f7d436a365aa12
bipineditz <bipineditz900@gmail.com>
2025-05-29 15:11:43 +0545

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,11 +6,11 @@ import os
 BOT_TOKEN = "7386912601:AAFiI5DWatlZvGnEdSW7g4UCT138jeIxARE"
 # From my.telegram.org/
 API_ID = "29437846"
-API_HASH =  ""
+API_HASH =  "af1b638c55e30104ded349d17adffc5f"
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "7071768332").split(" ")]
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [7071768332]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
 BOT_USERNAME='http://t.me/BipinLiverecordingbot' #bot username without@
