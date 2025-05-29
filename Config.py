351b7bacf99110c2e64a33bd8e5b766435cacc53
bipineditz <bipineditz900@gmail.com>
2025-05-29 15:02:12 +0545

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = ""
+BOT_TOKEN = "7386912601:AAFiI5DWatlZvGnEdSW7g4UCT138jeIxARE"
 # From my.telegram.org/
 API_ID = ""
 API_HASH =  ""
@@ -13,6 +13,6 @@ OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
 AUTH_USERS = [969084369, 2800583882]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='test' #bot username without@
+BOT_USERNAME='http://t.me/BipinLiverecordingbot' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
